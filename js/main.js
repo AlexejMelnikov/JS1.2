c025b370fe535ab9a18a16e070caf0b1e60b151f
Alex <alexejmelnikov@rambler.ru>
2018-08-08 20:23:56 +0300

    Homework 1

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ num = num + "";
 num=num[0]*num[1]*num[2]*num[3]*num[4];
 console.log(num);
 
-num=Math.pow(num,3)+"";
-alert(num[0]+num[1]+num[2]);
+num=Math.pow(num, 3) + "";
+alert(num[0] + num[1]);
